612af51ca6ac1546cde1d7c960d13d75fb31628d
pedropachecog <pmpg@hotmail.com>
2022-09-18 14:09:20 -0400

    After 2.2 video

--- app/src/main/java/com/example/flix/MainActivity.kt
+++ app/src/main/java/com/example/flix/MainActivity.kt
@@ -6,18 +6,22 @@ import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private const val TAG = "MainActivity"
-private const val NOW_PLAYING_URL = "https://api.themoviedb" +
-        ".org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+private const val NOW_PLAYING_URL =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 
 class MainActivity : AppCompatActivity() {
+
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val client = AsyncHttpClient()
-        client.get(NOW_PLAYING_URL,object: JsonHttpResponseHandler(){
+        client.get(NOW_PLAYING_URL, object : JsonHttpResponseHandler() {
             override fun onFailure(
                 statusCode: Int,
                 headers: Headers?,
@@ -29,7 +33,13 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG, "onSuccess $json")
-                val movieJsonArray = json.jsonObject.getJSONArray("results")
+                try {
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    Log.i(TAG, "Movie List: $movies")
+                } catch (e: JSONException){
+                    Log.e(TAG, "Encountered exception $e")
+                }
             }
 
         })
