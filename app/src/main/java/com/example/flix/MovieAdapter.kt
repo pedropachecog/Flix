bb853f921d94589098b1f7552811e243bfcbe728
pedropachecog <pmpg@hotmail.com>
2022-09-25 16:47:34 -0400

    Completed Part 1 of Flix 2

--- app/src/main/java/com/example/flix/MovieAdapter.kt
+++ app/src/main/java/com/example/flix/MovieAdapter.kt
@@ -1,15 +1,21 @@
 package com.example.flix
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.Placeholder
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import java.net.ConnectException
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+private const val TAG = "MovieAdapter"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) : RecyclerView
 .Adapter<MovieAdapter.ViewHolder>() {
@@ -28,10 +34,14 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
@@ -40,6 +50,20 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
             Glide.with(context).load(movie.posterImageUrl).centerCrop().placeholder(R
                 .drawable
                     .poster_placeholder).into(ivPoster)
+
+        }
+
+        override fun onClick(p0: View?) {
+            // 1. Get notified of the movie that was clicked
+
+            val movie = movies[adapterPosition]
+//            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            // 2. Open an intent to show its details
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
 
         }
     }
